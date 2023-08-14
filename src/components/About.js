582ea6a77792cf92c2f4da0f49622358742f7f86
Jeevan-G <gaddamjeevan001@gmail.com>
2023-08-15 02:07:32 +0530

    firstcommit

---
+++ src/components/About.js
@@ -0,0 +1,22 @@
+import User from "./User";
+import { Component } from "react";
+import UserContext from "../utils/UserContext";
+
+class About extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  
+  render() {
+    
+
+    return (
+      <div>
+        This is a personal project. Hehe 😗.
+      </div>
+    );
+  }
+}
+
+export default About;
